5f310214d0ac3514012cbcebdc96d5ac9ecf2a8e
BrandonTBaldwin <brandontbaldwin99@gmail.com>
2021-10-10 23:30:48 -0400

    test and file remove
    removed unneeded file
    fixed json test

--- Tests/Jsontests.java
+++ Tests/Jsontests.java
@@ -1,12 +1,11 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-
 public class Jsontests {
     @Test 
 	public void saveandLoadTestsBasic(){
 	
-			String s = "[{\"name\":\"a\",\"field\":[],\"method\":[{\"name\":\"b\",\"return_type\":\"int\",\"params\":[]}],\"rels\":[]}]";
+			String s = "[{\"name\":\"a\",\"fields\":[],\"methods\":[{\"name\":\"b\",\"return_type\":\"int\",\"params\":[]}],\"relationships\":[]}]";
             JsonFile.load(s, UML.getCollection());
 
             assertEquals("Doesn't work", s,  JsonFile.save(UML.getCollection()));
@@ -14,9 +13,9 @@ public class Jsontests {
     @Test
     public void saveAndLoadTestsAdvanced(){
 
-        
-        String s = "[{\"name\":\"alpha\",\"field\":[],\"method\":[{\"name\":\"method1\",\"return_type\":\"string\",\"params\":[{\"name\":\"x\",\"type\":\"int\"}]}],\"rels\":[{\"source\":\"alpha\",\"destination\":\"beta\",\"type\":\"aggregation\"},{\"source\":\"alpha\",\"destination\":\"delta\",\"type\":\"inheritance\"}]},{\"name\":\"beta\",\"field\":[],\"method\":[{\"name\":\"method2\",\"return_type\":\"int\",\"params\":[]}],\"rels\":[]},{\"name\":\"delta\",\"field\":[],\"method\":[],\"rels\":[]}]";
+        String s = "[{\"name\":\"alpha\",\"fields\":[],\"methods\":[{\"name\":\"method1\",\"return_type\":\"string\",\"params\":[{\"name\":\"x\",\"type\":\"int\"}]}],\"relationships\":[{\"source\":\"alpha\",\"destination\":\"beta\",\"type\":\"aggregation\"},{\"source\":\"alpha\",\"destination\":\"delta\",\"type\":\"inheritance\"}]},{\"name\":\"beta\",\"fields\":[],\"methods\":[{\"name\":\"method2\",\"return_type\":\"int\",\"params\":[]}],\"relationships\":[]},{\"name\":\"delta\",\"fields\":[],\"methods\":[],\"relationships\":[]}]";
         JsonFile.load(s, UML.getCollection());
+        
         assertEquals("Doesn't work", s,  JsonFile.save(UML.getCollection()));
     }
     
